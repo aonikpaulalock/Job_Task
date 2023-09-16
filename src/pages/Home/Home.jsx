e4784bcefbc67732af37411ed5aced6a3869323a
aonikpaulalock <alockpaul9845@gmail.com>
2023-09-16 13:03:45 +0600

    aded revolution section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 // import React from 'react';
 import Banner from './Banner';
 import Results from './Results';
+import Revolution from './Revolution';
 
 const Home = () => {
   return (
     <div>
       <Banner/>
       <Results/>
+      <Revolution/>
     </div>
   );
 };
--- src/pages/Home/Revolution.jsx
+++ src/pages/Home/Revolution.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import "../../assets/styles/home/revolution.css"
 const Revolution = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <div className='revolution-container'>
+        <div className="row">
+          <div className="col-md-8">
+            <div className="revolution-contain">
+              <h1 className='revolution-heading'>Join the demo experience revolution</h1>
+              <p className='revolution-title'>Demos have come a long way from the traditional product demo video consumers were once familiar with. Learn how interactive software demos can showcase your product in all its glory, revolutionizing the way you sell and transforming the way your customers buy. </p>
+            </div>
+          </div>
+          <div className="col-md-3 offset-1 d-flex justify-content-center align-items-center">
+            <button className="revolution-button">Book a Demo</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
